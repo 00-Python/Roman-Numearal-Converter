8bc62445fb1fe316b3efdf0c9f4ae16e8c29b745
Joseph Webster <josephwebstercolby@gmail.com>
2022-10-10 23:27:20 +0100

    final version

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        
-        broke_down = [*s] # make a list
+        doubles = {'IV':-1, 'IX':-1, 'XL':-10, 'XC':-10, 'CD':-100, 'CM':-100}
+        broke_down = [*s]  # make a list
+        total = []
+        length = len(broke_down)
 
-        nn = []
         for num in broke_down:
-            print(num)
             if num in nums:
-                nn.append(nums[num])
-        nn_length = int(len(nn))
-                 
-        total = 0 
-        print(nn)
-        for x in range(0, nn_length):
-            if x < len(nn)-1:
-                
-                if nn[x] == 1 and nn[x + 1] == 5 or nn[x] == 1 and nn[x + 1] == 10:
-                    nn.append(-1)
-                elif nn[x] == 10 and nn[x + 1] == 50 or nn[x] == 10 and nn[x + 1] == 100:
-                    nn.append(-10)
-                elif nn[x] == 100 and nn[x + 1] == 500 or nn[x] == 100 and nn[x + 1] == 1000:
-                    nn.append(-100)
-        
-        print(nn)
-                
-            
-s = 'MCMXCIV'        
+                total.append(nums[num])
+            else:
+                break
+
+        for double in s:
+            if double in s:
+                split = [*double]
+                s = s.replace(split[0], '')
+            else:
+                break
+
+
+        for x in range(0, length):
+            if x < length-1:
+                if broke_down[x] == 'I' and broke_down[x + 1] == 'V' or broke_down[x] == 'I' and broke_down[x + 1] == 'X':
+                    total.append(-1)
+                    total.remove(1)
+                elif broke_down[x] == 'X' and broke_down[x + 1] == 'L' or broke_down[x] == 'X' and broke_down[x + 1] == 'C':
+                    total.append(-10)
+                    total.remove(10)
+                elif broke_down[x] == 'C' and broke_down[x + 1] == 'D' or broke_down[x] == 'C' and broke_down[x + 1] == 'M':
+                    total.append(-100)
+                    total.remove(100)
+        return sum(total)
+
+
+s = 'MCMXCIV'
 x = Solution()
 conversion = x.romanToInt(s)
 
